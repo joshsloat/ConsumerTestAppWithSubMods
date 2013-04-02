b84dcaf2cc4bb640a699140d9f5604cea0046a7a
Josh Sloat <jsloat@articulate.com>
2013-04-02 11:15:44 -0400

    Initial Commit

---
+++ ConsumerTestAppWithSubMods/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ConsumerTestAppWithSubMods
+//
+//  Created by Josh Sloat on 4/2/13.
+//  Copyright (c) 2013 Josh Sloat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
