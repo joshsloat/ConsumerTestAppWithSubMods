756f50aef84835610404db61e8e4d7558494eb1a
Josh Sloat <jsloat@articulate.com>
2013-04-02 11:17:06 -0400

    Added test log statement.

--- ConsumerTestAppWithSubMods/ViewController.m
+++ ConsumerTestAppWithSubMods/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Todo: say hello world from lib code");
 }
 
 - (void)didReceiveMemoryWarning
